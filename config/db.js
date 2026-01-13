768801a5e035b9fd76620bfdfcb10c0c067dabd8
Baxtiyor <baxtiyorochilov2010@gmal.com>
2026-01-13 21:53:47 +0500

    Final change 2 I think

--- config/db.js
+++ config/db.js
@@ -6,15 +6,13 @@ const connectDB = async () => {
       throw new Error("MONGO_URI .env faylida topilmadi!");
     }
 
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    });
+    // Mongoose 7 da useNewUrlParser va useUnifiedTopology kerak emas
+    await mongoose.connect(process.env.MONGO_URI);
 
     console.log("MongoDB connected ✅");
   } catch (error) {
     console.error("MongoDB connection error ❌:", error.message);
-    process.exit(1); // serverni to‘xtatadi
+    process.exit(1);
   }
 };
 
