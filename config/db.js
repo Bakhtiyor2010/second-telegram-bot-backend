b4da5656247c640782fb70240c959df1468488f8
Baxtiyor <baxtiyorochilov2010@gmal.com>
2026-01-15 18:33:12 +0500

    console

--- config/db.js
+++ config/db.js
@@ -6,6 +6,7 @@ const serviceAccount = {
   clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
   privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
 };
+console.log("FIREBASE_PRIVATE_KEY:", process.env.FIREBASE_PRIVATE_KEY ? "FOUND" : "NOT FOUND");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
